1b6d92a3d1bf4851a0c7e4d6ec5fdb9187d830f8
ptrwn <1374688@gmail.com>
2023-05-04 23:32:00 +0300

    added tasks into db

--- db.py
+++ db.py
@@ -107,8 +107,16 @@ def fill_in_db():
         f_1 = UserFlat(user=user_f, flat=flat_1, user_role="Guest")
 
         t_a_b_1 = Tasks(name="dishes", creator_id=1, assignee_id=2, flat_id=1)
+        t_b_c_1 = Tasks(name="dust", creator_id=2, assignee_id=3, flat_id=1)
+        t_c_d_1 = Tasks(name="take out garbage", creator_id=3, assignee_id=4, flat_id=1)
+        t_d_f_1 = Tasks(name="cook", creator_id=4, assignee_id=6, flat_id=1)
+        t_a_c_1 = Tasks(name="walk the dog", creator_id=1, assignee_id=3, flat_id=1)
+        t_b_d_1 = Tasks(name="clean cat toilet", creator_id=2, assignee_id=4, flat_id=1)
+        t_c_a_1 = Tasks(name="washing", creator_id=3, assignee_id=1, flat_id=1)
+        t_e_f_2 = Tasks(name="iron curtains", creator_id=5, assignee_id=6, flat_id=2)
 
-        session.add_all([user_a, user_b, user_c, user_d, user_e, user_f, flat_1, flat_2, a_1, b_1, c_1, d_1, f_1, e_2, f_2, t_a_b_1])
+
+        session.add_all([user_a, user_b, user_c, user_d, user_e, user_f, flat_1, flat_2, a_1, b_1, c_1, d_1, f_1, e_2, f_2, t_a_b_1, t_b_c_1, t_c_d_1, t_d_f_1, t_a_c_1, t_b_d_1, t_c_a_1, t_e_f_2])
 
         session.commit()
 
